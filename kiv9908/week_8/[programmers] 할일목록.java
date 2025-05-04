85711e800d984f2eb393f366d834c13153a51032
kevinlee <105219462+kiv9908@users.noreply.github.com>
2025-05-04 23:56:15 +0900

    Add: [programmers] 할일목록.java

---
+++ kiv9908/week_8/[programmers] 할일목록.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+	public String[] solution(String[] todo_list, boolean[] finished) {
+		
+		List<String> list = new ArrayList<>();
+		
+		for (int i = 0 ;i<finished.length;i++) {
+			if(!finished[i]) {
+				list.add(todo_list[i]);
+			}
+		}
+		
+		String[] answer = new String[list.size()];
+		for (int i = 0;i<list.size();i++) {
+			answer[i] = list.get(i);
+		}
+		
+		return answer;
+	}
+}
