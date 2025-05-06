a0e1c6843a924595a0c410bb0891ce5fb482eecf
kevinlee <105219462+kiv9908@users.noreply.github.com>
2025-05-06 16:19:31 +0900

    Add: [programmers] A강조하기.java

---
+++ kiv9908/week_9/[programmers] A강조하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i = 0;i<myString.length();i++) {
+        	if("a".equals(""+myString.charAt(i))){
+        		answer+=String.valueOf(myString.charAt(i)).toUpperCase();
+        	}else if(!"A".equals(""+myString.charAt(i))) {
+        		answer+=(""+myString.charAt(i)).toLowerCase();
+        		
+        	}
+        	
+        	else {
+        		answer+=myString.charAt(i);
+        	}
+        }
+        
+        return answer;
+    }
+}
