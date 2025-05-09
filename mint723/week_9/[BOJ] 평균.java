429debc232931b95fc182d56622d22e953dd6dce
mint723 <114153768+mint723@users.noreply.github.com>
2025-05-10 01:16:34 +0900

    Add: [BOJ] 평균.java

---
+++ mint723/week_9/[BOJ] 평균.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[] scores = new int[num];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = 0;
+
+        for (int i = 0; i < num; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < num; i++) {
+            sum += (double) scores[i] / max * 100;
+        }
+
+        System.out.println(sum / num);
+    }
+}
